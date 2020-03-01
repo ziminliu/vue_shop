6021fedabf870f14fa660e03201c1851e734f4e4
ziminliu <liuziminliuzimin@gmail.com>
2020-03-01 23:16:42 +0800

    Home 页面布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header class="el-header">
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 页面侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="white"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template></el-menu-item
+            >
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧页面内容 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -13,4 +49,31 @@ export default {
   }
 }
 </script>
-<style></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eee;
+}
+</style>
