77bdee782c2ac563d759b51a5565388541b08497
ziminliu <liuziminliuzimin@gmail.com>
2020-03-02 12:53:29 +0800

    子路由welcome

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -47,7 +47,9 @@
         </el-menu>
       </el-aside>
       <!-- 右侧页面内容 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome }]
+  }
 ]
 
 const router = new VueRouter({
@@ -17,7 +23,7 @@ const router = new VueRouter({
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-/*  to 将要访问的路径
+  /*  to 将要访问的路径
     from 来自的路径
     next 是一个函数，表示放行
     next()  放行     next('login') 强制跳转
