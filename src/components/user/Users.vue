d8ab89ba105233092a666b9d136c5ae80487457e
ziminliu <liuziminliuzimin@gmail.com>
2020-03-07 22:06:49 +0800

    调用API完成用户添加

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -11,10 +11,11 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="20">
         <el-col :span="7"
-          ><el-input placeholder="请输入内容"
-          v-model="queryInfo.query"
-          clearable
-          @clear="getUserList"
+          ><el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
           >
             <el-button
               slot="append"
@@ -23,7 +24,9 @@
             ></el-button> </el-input
         ></el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true"
+            >添加用户</el-button
+          >
         </el-col>
       </el-row>
       <!-- 用户列表区域 -->
@@ -80,12 +83,61 @@
       >
       </el-pagination>
     </el-card>
+    <!-- 添加用户的对话框 -->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
+      <!-- 内容主体区域 -->
+      <el-form
+        ref="addFormRef"
+        :model="addForm"
+        :rules="addFormRules"
+        label-width="70px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input> </el-form-item
+        ><el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input> </el-form-item
+        ><el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input> </el-form-item
+        ><el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   data() {
+    // 验证邮箱规则
+    var checkEmail = (rule, value, cb) => {
+      // 验证邮箱规则的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+([a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        // 合法邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法邮箱'))
+    }
+    var checkMobile = (rule, value, cb) => {
+      // 验证邮箱规则的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        // 合法邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法手机号'))
+    }
     return {
       // 获取用户列表的参数对象
       queryInfo: {
@@ -94,7 +146,66 @@ export default {
         pagesize: 2
       },
       userList: [],
-      total: 0
+      total: 0,
+      // 控制添加用户对话框的显示与隐藏
+      addDialogVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '用户名的长度在3到10个之间',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 10,
+            message: '密码的长度在6到10个之间',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur'
+          },
+          {
+            validator: checkEmail,
+            trigger: 'blur'
+          }
+        ],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机号',
+            trigger: 'blur'
+          },
+          {
+            validator: checkMobile,
+            trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   created() {
@@ -133,6 +244,25 @@ export default {
         return this.$message.error('更新状态失败')
       }
       this.$message.success('更新状态成功')
+    },
+    // 关闭重置表单
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮,提交表单
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return
+        // 可以通过API 添加用户
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) {
+          this.$message.error('用户添加失败')
+        }
+        this.$message.success('添加用户成功')
+        this.addDialogVisible = false
+        // 重新获取用户列表
+        this.getUserList()
+      })
     }
   }
 }
